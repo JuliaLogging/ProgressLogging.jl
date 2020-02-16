5466bf5b8a3400a9163943e26c79ded921b811b1
Takafumi Arakaki <aka.tkf@gmail.com>
2020-02-16 15:28:47 -0800

    Don't fail when fraction::Int (#20)

--- test/test_progress_record.jl
+++ test/test_progress_record.jl
@@ -5,6 +5,13 @@ using Random: shuffle!
 using Test
 using UUIDs: uuid4
 
+@testset "fraction :: Int" begin
+    @test Progress(id = uuid4(), fraction = 0).fraction === 0.0
+    @test Progress(id = uuid4(), fraction = 1).fraction === 1.0
+    @test Progress(uuid4(), 0).fraction === 0.0
+    @test Progress(uuid4(), 1).fraction === 1.0
+end
+
 @testset "cmp: ProgressString" begin
     strings = Any[
         let s = string(i)
